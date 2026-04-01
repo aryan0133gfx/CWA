4303ef7cd764b8244eddb2820413828df9b6ca36
aryan jadhav <aryanjadhav@aryans-MacBook-Air-3.local>
2026-04-01 14:12:32 +0530

    Logic and html structure

--- app.py
+++ app.py
@@ -1,44 +1,61 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-CORS(app)
 
 patients = []
 
-def get_priority(data):
-    age = int(data["age"])
-    severity = int(data["severity"])
-    symptom = data["symptom"]
-    duration = int(data["duration"])
+def get_priority(age, symptom, severity, duration):
+    age = int(age)
+    severity = int(severity)
+    duration = int(duration)
+    symptom = symptom.lower()
 
-    if symptom == "chest pain" and age > 40:
-        return 3, "Chest pain + age risk"
-    elif severity >= 8:
-        return 3, "High severity"
-    elif symptom == "fever" and duration > 3:
-        return 2, "Prolonged fever"
-    else:
-        return 1, "Normal condition"
+    if symptom in ["chest pain", "heart pain"]:
+        return 3, "Critical: chest pain, Dr.Ajjenkya pawar,Cardiologist"
 
-@app.route("/add", methods=["POST"])
-def add_patient():
-    data = request.json
+    if symptom in ["breathing problem", "shortness of breath"]:
+        return 3, "Critical: breathing issue, Dr.Suman Jadhav,Pulmonologist"
 
-    priority, reason = get_priority(data)
+    if severity >= 8:
+        return 3, "High severity, ICU Recomandation"
+    
+    if age < 8:
+        return 3, "Emergency, Dr.Aaditi Mestry,Pediatrician"
 
-    patient = {
-        "name": data["name"],
-        "priority": priority,
-        "reason": reason
-    }
+    if symptom in ["fever"] and duration > 3:
+        return 2, "Prolonged fever,Dr.Chandan Pardeshi,General physician"
 
-    patients.append(patient)
+    if symptom in ["vomiting", "dizziness"]:
+        return 2, "Moderate risk symptoms,Dr.Manisha Pardeshi,General physician"
 
-    # sort by priority
-    patients.sort(key=lambda x: x["priority"], reverse=True)
+    if age > 60 and severity > 5:
+        return 2, "Age risk, Dr.Balaji Rupnar,Geriatrician"
+    
+    
 
-    return jsonify(patients)
+    return 1, "Stable condition, OPD Recomadition"
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form["name"]
+        age = request.form["age"]
+        symptom = request.form["symptom"]
+        severity = request.form["severity"]
+        duration = request.form["duration"]
+
+        priority, reason = get_priority(age, symptom, severity, duration)
+
+        patient = {
+            "name": name,
+            "priority": priority,
+            "reason": reason
+        }
+
+        patients.append(patient)
+        patients.sort(key=lambda x: x["priority"], reverse=True)
+
+    return render_template("index.html", patients=patients)
 
 if __name__ == "__main__":
     app.run(debug=True)
